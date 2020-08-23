86a3f9f92948a1bd54697641c3d8f28b98138860
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-08-23 19:37:14 +0530

    App.js updated

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 let app = express();
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
 
 // Server static assets if in production
-if (true) {
-  // Set static folder
-  app.use(express.static("build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 const port = process.env.PORT || "8080";
 app.set("port", port);
