33dce57ddc39e6c67a456edf26e23c266f1d680e
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-08-23 19:32:46 +0530

    App.js updated

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 let app = express();
-app.use(express.static(path.join(__dirname, "build")));
+
+// Server static assets if in production
+if (true) {
+  // Set static folder
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || "8080";
 app.set("port", port);
 const server = http.createServer(app);
